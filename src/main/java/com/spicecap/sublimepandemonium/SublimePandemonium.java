e1c18657f28c3be262f2932956eee4018f925f7e
Capde <103727616+CapdeO@users.noreply.github.com>
2023-01-12 23:30:23 -0300

    Se me armó quilombo

--- src/main/java/com/spicecap/sublimepandemonium/SublimePandemonium.java
+++ src/main/java/com/spicecap/sublimepandemonium/SublimePandemonium.java
@@ -16,11 +16,28 @@ public class SublimePandemonium {
         
         ArrayList<Card> deck1 = new ArrayList<Card>(5); 
         
+        deck1.add(new Card(0, Card.Type.TANK, "Taur", 12, 1, Card.Habbility.HEAL_SLF));
+        deck1.add(new Card(2, Card.Type.FIGHTER, "Abomination", 7, 5, Card.Habbility.DMG_UP));
+        deck1.add(new Card(4, Card.Type.SUPPORT, "Rafaela", 10, 1, Card.Habbility.HEAL_2));
+        deck1.add(new Card(1, Card.Type.MAGE, "Ciclops", 5, 2, Card.Habbility.STORM));
+        deck1.add(new Card(3, Card.Type.MARKSMAN, "Elf", 4, 6, Card.Habbility.DMG_CRIT));
+        
+        
         
         ArrayList<Card> deck2 = new ArrayList<Card>(5); 
         
+        deck2.add(new Card(0, Card.Type.TANK, "Taur", 12, 1, Card.Habbility.HEAL_SLF));
+        deck2.add(new Card(2, Card.Type.FIGHTER, "Abomination", 7, 5, Card.Habbility.DMG_UP));
+        deck2.add(new Card(4, Card.Type.SUPPORT, "Rafaela", 10, 1, Card.Habbility.HEAL_2));
+        deck2.add(new Card(1, Card.Type.MAGE, "Ciclops", 5, 2, Card.Habbility.STORM));
+        deck2.add(new Card(3, Card.Type.MARKSMAN, "Elf", 4, 6, Card.Habbility.DMG_CRIT));
         
+        //--------------------------------------------------------
         
+        System.out.println("DECK 1");
+        System.out.println(deck1);
+        System.out.println("DECK 2");
+        System.out.println(deck2);
         
     }
 }
