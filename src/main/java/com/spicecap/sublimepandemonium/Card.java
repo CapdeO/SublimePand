754365cef1848d2cf91d7d773402444343d35b1f
Capde <103727616+CapdeO@users.noreply.github.com>
2023-02-01 16:22:53 -0300

    Add files via upload

--- src/main/java/com/spicecap/sublimepandemonium/Card.java
+++ src/main/java/com/spicecap/sublimepandemonium/Card.java
@@ -12,31 +12,31 @@ public class Card implements Cloneable{
     public enum Habbility {
         BLEED_ONE_1,      // %35 chance - 2 turns
         BLEED_ONE_2,      // %25 chance - 4 turns
-        BLEED_TWO_1,      // %30 chance - 2 turns
-        BLEED_TWO_2,      // %25 chance - 3 turns
+        BLEED_TWO_1,      // %25 chance - 2 turns
+        BLEED_TWO_2,      // %20 chance - 3 turns
         
-        CLEAN_ONE_1,      // % chance -  turns
-        CLEAN_ONE_2,      // % chance -  turns
-        CLEAN_TWO_1,      // % chance -  turns
-        CLEAN_TWO_2,      // % chance -  turns
+        CLEAN_ONE_1,      // %40 chance - 2 pts heal
+        CLEAN_ONE_2,      // %35 chance - 3 pts heal
+        CLEAN_TWO_1,      // %25 chance - 2 pts heal
+        CLEAN_TWO_2,      // %20 chance - 3 pts heal
         
-        DMG_CRIT_1,       // % chance -  turns
-        DMG_CRIT_2,       // % chance -  turns
+        DMG_CRIT_1,       // %25 chance - 3 pts
+        DMG_CRIT_2,       // %20 chance - 4 pts
         
-        DMG_RED_1,        // % chance -  turns
-        DMG_RED_2,        // % chance -  turns
+        DMG_RED_1,        // %25 chance - 2 pts
+        DMG_RED_2,        // %20 chance - 3 pts
         
-        DMG_REF_1,        // % chance -  turns
-        DMG_REF_2,        // % chance -  turns
+        DMG_REF_1,        // %50 chance - %30 dmg
+        DMG_REF_2,        // %30 chance - %50 dmg
         
-        DMG_UP_SLF_1,     // % chance -  turns
-        DMG_UP_SLF_2,     // % chance -  turns
+        DMG_UP_SLF_1,     // %25 chance - 2 pts
+        DMG_UP_SLF_2,     // %20 chance - 3 pts
         
-        DODGE_1,          // % chance -  turns
-        DODGE_2,          // % chance -  turns
+        DODGE_1,          // %20 chance 
+        DODGE_2,          // %30 chance 
         
-        FINISH_1,         // % chance -  turns
-        FINISH_2,         // % chance -  turns
+        FINISH_1,         // %25 chance - 6 pts
+        FINISH_2,         // %20 chance - 7 pts
         
         FIRE_ONE_1,       // %35 chance - 3 turns
         FIRE_ONE_2,       // %30 chance - 4 turns
@@ -57,6 +57,9 @@ public class Card implements Cloneable{
         
         STUN_TWO_1,       // %20 chance - 1 turns
         STUN_TWO_2,       // %15 chance - 2 turns
+        
+        POISON_1,         // %40  chance
+        POISON_2,         // %30  chance
     }
     
     public int id;
@@ -72,6 +75,8 @@ public class Card implements Cloneable{
     public int stuned;
     public int bleeding;
     public int fire;
+    
+    public boolean poisoned;
     
     public boolean dead;
     
@@ -125,16 +130,21 @@ public class Card implements Cloneable{
             case THUNDER_ONE_2 -> thunderOne(deckDest, 30, 4);
             case THUNDER_TWO_1 -> thunderTwo(deckDest, 20, 3);
             case THUNDER_TWO_2 -> thunderTwo(deckDest, 30, 2);
-            
-            case FINISH_1 -> finishOne(deckDest, 25, 7);
+            case FINISH_1 -> finishOne(deckDest, 25, 6);
+            case FINISH_2 -> finishOne(deckDest, 20, 7);
         // BUFF --------------    
-            case DMG_UP_SLF_1 -> damageUpSelf(this, 30, 3);
+            case DMG_UP_SLF_1 -> damageUpSelf(this, 25, 2);
+            case DMG_UP_SLF_2 -> damageUpSelf(this, 20, 3);
             case DMG_CRIT_1 -> attackPoints += damageCrit(25, 3);
+            case DMG_CRIT_2 -> attackPoints += damageCrit(20, 4);
             case HEAL_SLF_1 -> healSelf(this, 25, 4);
             case HEAL_SLF_2 -> healSelf(this, 30, 2);
             case HEAL_TWO_1 -> healTwo(deckOrig, 20, 3);
-            case HEAL_TWO_2 -> healTwo(deckDest, 25, 2);
-            case CLEAN_ONE_1 -> cleanOne(deckOrig, 50);
+            case HEAL_TWO_2 -> healTwo(deckOrig, 25, 2);
+            case CLEAN_ONE_1 -> cleanOne(deckOrig, 40, 2);
+            case CLEAN_ONE_2 -> cleanOne(deckOrig, 35, 3);
+            case CLEAN_TWO_1 -> cleanTwo(deckDest, 25, 2);
+            case CLEAN_TWO_2 -> cleanTwo(deckDest, 20, 3);
         // DEBUFF -------------    
             case STUN_ONE_1 -> stunOne(deckDest, 20, 2);
             case STUN_ONE_2 -> stunOne(deckDest, 25, 1);
@@ -161,16 +171,18 @@ public class Card implements Cloneable{
                 target++;
             } else {
                     
-                if (deckDest[target].habbility == Habbility.DMG_RED_1) {
-                    attackPoints -= damageReduc(25, 2);
-                    if (attackPoints < 0) 
+                if (deckDest[target].habbility == Habbility.DMG_RED_1) 
+                    attackPoints -= damageReduc(25, 2); 
+                else if (deckDest[target].habbility == Habbility.DMG_RED_2)
+                    attackPoints -= damageReduc(20, 3);
+                
+                if (attackPoints < 0) 
                         attackPoints = 0;
-                }  
                 
                 
                 switch (deckDest[target].habbility) {
                     case DMG_REF_1 -> {
-                        if (posibilidad() <= 70) {
+                        if (posibilidad() <= 50) {
                             basicAttack(this, deckDest[target], (int) Math.round(attackPoints * 0.7));
                             System.out.println("Damage reflection ("+ (int) Math.floor(attackPoints * 0.34) + "pt)");
                             this.hp -= Math.floor(attackPoints * 0.34);
@@ -182,8 +194,32 @@ public class Card implements Cloneable{
                         }
                     }
                     
+                    case DMG_REF_2 -> {
+                        if (posibilidad() <= 30) {
+                            basicAttack(this, deckDest[target], (int) Math.round(attackPoints * 0.5));
+                            System.out.println("Damage reflection ("+ (int) Math.round(attackPoints * 0.5) + "pt)");
+                            this.hp -= Math.round(attackPoints * 0.5);
+                            checkDeath(this);
+                            attackDone = true;
+                        } else {
+                            basicAttack(this, deckDest[target], attackPoints);
+                            attackDone = true;
+                        }
+                    }
+                    
                     case DODGE_1 -> {
                         if (posibilidad() <= 20) {
+                            System.out.println(this + "@" + Integer.toHexString(this.hashCode()) + " ATTACKED--> " + deckDest[target] + "@" + Integer.toHexString(deckDest[target].hashCode()));
+                            System.out.println("But " + deckDest[target] + " dodged his attack");
+                            attackDone = true;
+                        } else {
+                            basicAttack(this, deckDest[target], attackPoints);
+                            attackDone = true;
+                        }
+                    }
+                    
+                    case DODGE_2 -> {
+                        if (posibilidad() <= 30) {
                             System.out.println(this + "@" + Integer.toHexString(this.hashCode()) + " ATTACKED--> " + deckDest[target] + "@" + Integer.toHexString(deckDest[target].hashCode()));
                             System.out.println("But " + deckDest[target] + " dodged his attack");
                             attackDone = true;
@@ -416,28 +452,76 @@ public class Card implements Cloneable{
         System.out.println(card + " is now clean");
     }
     
-    public static void cleanOne(Card[] deck, int chance) {
-        int[] array = new int[5];
-        int puntero = 0;
-        
-        for (int i = 0; i < 5; i++) {
-            if (deck[i].isStuned() || deck[i].isBleeding() || deck[i].isBurning()) {
-                array[puntero] = i;
-                puntero++;
+    public static void cleanOne(Card[] deck, int chance, int heal) {
+        if (posibilidad() <= chance) {
+            int[] array = new int[5];
+            int puntero = 0;
+
+            for (int i = 0; i < 5; i++) {
+                if (deck[i].isStuned() || deck[i].isBleeding() || deck[i].isBurning()) {
+                    if (!deck[i].dead) {
+                        array[puntero] = i;
+                        puntero++; 
+                    }
+                }
             }
+
+            puntero -= 1;
+
+            if (puntero == 0) {
+                clean(deck[array[0]]);
+                heal(deck[array[0]], heal);
+            } else if (puntero > 0) {
+                Random ran = new Random();
+                int afortunado = ran.nextInt(puntero) + 0;
+                clean(deck[array[afortunado]]);
+                heal(deck[array[afortunado]], heal);
+            }   
         }
-        
-        puntero -= 1;
+         
+    }
+    
+    public static void cleanTwo(Card[] deck, int chance, int heal) {
+        if (posibilidad() <= chance) {
                 
-        if (puntero == 0) {
-            clean(deck[array[0]]);
-            heal(deck[array[0]], 2);
-        } else if (puntero > 0) {
-            Random ran = new Random();
-            int afortunado = ran.nextInt(puntero) + 0;
-            clean(deck[array[afortunado]]);
-            heal(deck[array[afortunado]], 2);
-        }   
+            int[] array = new int[5];
+            int puntero = 0;
+                
+            for (int i = 0; i < 5; i++) {
+                if (deck[i].isStuned() || deck[i].isBleeding() || deck[i].isBurning()) {
+                    if (!deck[i].dead) {
+                        array[puntero] = i;
+                        puntero++; 
+                    }
+                }
+            }
+                
+            puntero -= 1;
+                
+            if (puntero == 0) {
+                clean(deck[array[0]]);
+                heal(deck[array[0]], heal);
+            } else if (puntero == 1) {
+                clean(deck[array[0]]);
+                heal(deck[array[0]], heal);
+                clean(deck[array[1]]);
+                heal(deck[array[1]], heal);
+            } else if (puntero > 1) {
+                    
+                Random ran = new Random();
+                int afortunado = ran.nextInt(puntero) + 0;
+                int afortunado2;
+                    
+                do {    
+                    afortunado2 = ran.nextInt(puntero) + 0;
+                } while (afortunado2 == afortunado);
+                
+                clean(deck[array[afortunado]]);
+                heal(deck[array[afortunado]], heal);
+                clean(deck[array[afortunado2]]);
+                heal(deck[array[afortunado2]], heal);
+            }    
+        }
     }
     
     // Debuff
@@ -595,6 +679,19 @@ public class Card implements Cloneable{
         System.out.println(attacker + "@" + Integer.toHexString(attacker.hashCode()) + " ATTACKED--> " + target + "@" + Integer.toHexString(target.hashCode()));
         target.hp -= points;
         checkDeath(target);
+        
+        if (!target.dead && attacker.habbility == Habbility.POISON_1) {
+            if (posibilidad() <= 40) {
+                target.poisoned = true;
+                System.out.println(target + " is poisoned");
+            }
+        } else if (!target.dead && attacker.habbility == Habbility.POISON_2) {
+            if (posibilidad() <= 30) {
+                target.poisoned = true;
+                System.out.println(target + " is poisoned");
+            }
+        }
+        
     }
     
     
